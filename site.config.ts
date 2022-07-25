bc531215c30f28d02e04b1e879f83e1e5a82dc00
ZXJ <50874494+ZXJNVRGVP@users.noreply.github.com>
2022-07-25 21:44:05 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '0760a2b4f2074cb09ca6c099770a7ba8',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
@@ -10,7 +10,7 @@ export default siteConfig({
 
   // basic site info (required)
   name: 'Hansire Blog',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
+  domain: 'https://www.notion.so/Hansire-Blog-0760a2b4f2074cb09ca6c099770a7ba8',
   author: 'Hans Zhang',
 
   // open graph metadata (optional)
